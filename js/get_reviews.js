66f4dfcb2bc245faa329f5ca0436612f0f3541bc
Daniel Jones <tortxof@gmail.com>
2015-05-22 00:27:43 -0400

    Styled review classes. Added Testimonials page to nav.

--- js/get_reviews.js
+++ js/get_reviews.js
@@ -4,7 +4,6 @@ $.getJSON("/testimonials/reviews.json", function(json) {
   var template = Handlebars.compile(source);
   for (var i in json) {
     var new_review = template(json[i]);
-    console.log(new_review);
     $("#reviews").append(new_review);
   }
 });
